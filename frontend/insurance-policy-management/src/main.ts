8aeafeeb866d5a01b1e5f3c5a2f53f5318d54b48
gowthamiseelam <gowthamiseelam2@gmail.com>
2025-06-04 12:53:18 +0530

    Cart and Toast Messages

--- frontend/insurance-policy-management/src/main.ts
+++ frontend/insurance-policy-management/src/main.ts
@@ -3,7 +3,11 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 
 bootstrapApplication(AppComponent, appConfig) 
   .catch((err) => console.error(err));
+
+
+
